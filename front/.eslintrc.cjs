82ad854b604d3c73b48ff048e3e218484497300a
Dmitrii Morrows <trollman1993@gmail.com>
2024-08-04 23:00:38 +0600

    update

---
+++ front/.eslintrc.cjs
@@ -0,0 +1,20 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/attribute-hyphenation': ['error', 'always'],
+    'vue/multi-word-component-names': 'off',
+    quotes: ['warn', 'single']
+  }
+}
---
+++ front/src/views/home-page.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+dasdsadas
+  </main>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
